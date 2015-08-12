5a5fbff23d0e01db17c69bb8ca419cd09a469e50
Chris Liow <c.liow@claritypartners.com>
2015-08-11 20:56:56 -0500

    Basic script to read file and filter

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-print('Test')
+from openpyxl import load_workbook
+
+wb = load_workbook('Sept 15 Cal.xlsx')
+sheet = wb['Sept 2015']
+
+for i in range(1,200):
+	if sheet['D' + str(i)].value != None:
+		if ('P' in sheet['D' + str(i)].value) and (sheet['H' + str(i)].value == 'FIRM'):
+			print(i)
+	
